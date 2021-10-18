e1dda06959bd8ffab8487db93e9ef1cd0654911b
CsignS <smileroom2010@gmail.com>
2021-10-18 17:23:21 +0700

    Ldap

---
+++ src/main/java/com/example/ldappixeltrice/LdapPixeltriceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ldappixeltrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapPixeltriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdapPixeltriceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ldappixeltrice/LdapPixeltriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ldappixeltrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapPixeltriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
